3a1a3ad57582fc81accf9f2630201f00163b99b4
agnelo000 <agnelo000@gmail.com>
2020-02-17 20:06:51 +0000

    first commit

---
+++ hello.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+
+
+posts = [
+    {
+        'author': 'Agnelo',
+        'title': 'how to alienate people',
+        'content': 'shitpost',
+        'date_published': 'jan 30'
+    },
+    {
+        'author': 'Vikram singh',
+        'title': 'bobs and vagena',
+        'content': 'shitpost',
+        'date_published': 'feb 30'
+
+    }
+]
+app = Flask(__name__)
+
+@app.route("/")
+
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About Page')
+
+if (__name__ == "__main__"):
+    app.run(debug=True)
